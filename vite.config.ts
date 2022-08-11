3178ae8f73fe128b2d66c3b89c4ed40d4f9b34b7
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2022-08-11 18:23:12 +0700

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { VitePWA } from 'vite-plugin-pwa';
 import * as path from 'path';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
